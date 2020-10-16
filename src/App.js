3b358ea605392a3a2e88a206727122d9c1d63c0c
Leonardo <leonardo.e.acosta@outlook.com>
2020-10-16 09:59:11 -0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Windrunner from "./Windrunner";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <div className="jumbotron splashImage" >
+        <div className="container text-center">
+          <h1 className="display-4">Claire's Closet Cosplay</h1>
+          <p className="text-white"><i class="fas fa-angle-double-down"></i>River Rifley</p>
+          
+        </div>
+      </div>
+      <div className="divider"></div>
+      <Windrunner/>
     </div>
   );
 }
---
+++ src/Windrunner/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Main from './Main.jpg';
+
+export default function Windrunner(){
+    return(
+        <div className="bg-dark text-white">
+            <img src={Main} className="w-100"/>
+        </div>
+    )
+}
